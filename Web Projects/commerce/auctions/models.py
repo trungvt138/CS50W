f8e763643ef7ad963529f0ebcd6e76eea84a3de3
trung <trungvt138@github.com>
2024-09-05 11:42:53 +0200

    create Listing Comment and Bid models

--- Web Projects/commerce/auctions/models.py
+++ Web Projects/commerce/auctions/models.py
@@ -3,4 +3,32 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=50, blank=True, null=False)
+    last_name = models.CharField(max_length=50, blank=True, null=False)
+
+
+class Bid(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Listing(models.Model):
+    listing_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    bid = models.ForeignKey(Bid, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(Comment)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
